55dd3c9f18fd67373137cea25ef1cfac711cea17
Sergio Gomez <sergio@pasalapagina.com>
2024-12-10 14:35:53 -0500

    feat: table pages for adding new columns

--- src/app/calculadora/page.tsx
+++ src/app/calculadora/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navigation/Navbar";
 import DyeInputs from "@/components/tables/DyeInputs";
 import FraganceInputs from "@/components/tables/FraganceInputs";
 import JarInputs from "@/components/tables/JarInputs";
@@ -10,66 +11,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
 export default function page() {
   return (
     <main className="min-h-svh w-full">
-      <nav className="px-20 py-4">
-        <h1>Sui</h1>
-      </nav>
+      <Navbar />
       <section className="grid grid-cols-2 gap-4 px-10">
         <Card>
           <CardHeader>
---
+++ src/components/navigation/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="px-20 py-4">
+      <h1>Sui</h1>
+    </nav>
+  );
+}
