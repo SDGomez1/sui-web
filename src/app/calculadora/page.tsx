3c06dade05b227fea2f94fadd013861c0d3928cc
Sergio Gomez <sergio@pasalapagina.com>
2024-12-13 17:13:00 -0500

    feat: table card for improvemente in organization

--- src/app/calculadora/page.tsx
+++ src/app/calculadora/page.tsx
@@ -1,68 +1,10 @@
+import TableCards from "@/components/calculator/TableCards";
 import Navbar from "@/components/navigation/Navbar";
-import DyeInputs from "@/components/tables/DyeInputs";
-import FraganceInputs from "@/components/tables/FraganceInputs";
-import JarInputs from "@/components/tables/JarInputs";
-import StickerTagInputs from "@/components/tables/StickerTagInputs";
-import WaxInputs from "@/components/tables/WaxInputs";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
 export default function page() {
   return (
-    <main className="min-h-svh w-full">
+    <main className="min-h-svh w-full pb-10">
       <Navbar />
-      <section className="grid grid-cols-2 gap-4 px-10">
-        <Card>
-          <CardHeader>
-            <CardTitle>Cera</CardTitle>
-            <CardDescription>Todas las ceras disponibles</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <WaxInputs />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Tintes</CardTitle>
-            <CardDescription>Todos las tintes disponibles</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <DyeInputs />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Fragancias</CardTitle>
-            <CardDescription>Todas las fragancias disponibles</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <FraganceInputs />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Tarros</CardTitle>
-            <CardDescription>Todos los tarros disponibles</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <JarInputs />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Etiquetas</CardTitle>
-            <CardDescription>Todas las etiquetas disponibles</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <StickerTagInputs />
-          </CardContent>
-        </Card>
-      </section>
+      <TableCards />
     </main>
   );
 }
